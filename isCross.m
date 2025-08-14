27bd09e8aa1b5092bdc3de6131f66ff4de76616d
lihonglei327 <88067069+lihonglei327@users.noreply.github.com>
2025-08-14 18:59:23 +0800

    Update isCross.m

--- isCross.m
+++ isCross.m
@@ -11,9 +11,22 @@ function f=isCross(s1,s2)
     f=0;
     try
         bounds1=[min(s1);max(s1)];
+        bounds2=[min(s2);max(s2)];         
+
+        %shrink the observing zone
+        bounds=[max([bounds1(1,:);bounds2(1,:)]);min([bounds1(2,:);bounds2(2,:)])];
+        
+        %shrink the sequences
+        s1=s1(s1(:,1)>=bounds(1,1)  & s1(:,1)<=bounds(2,1) ,:);
+        s2=s2(s2(:,1)>=bounds(1,1)  & s2(:,1)<=bounds(2,1) ,:);
+        
+        %refresh the bounds of sequences
+        bounds1=[min(s1);max(s1)];
         bounds2=[min(s2);max(s2)];
-        y_cross=prod(bounds2(:,2)-bounds1(:,2))<0;
+       
         x_cross=min(bounds1(:,1))-max(bounds2(:,1))<0 && min(bounds2(:,1))-max(bounds1(:,1))<0;
+        y_cross=prod(bounds2(:,2)-bounds1(:,2))<0;
+
         f=y_cross*x_cross;
     catch
         f=0;
